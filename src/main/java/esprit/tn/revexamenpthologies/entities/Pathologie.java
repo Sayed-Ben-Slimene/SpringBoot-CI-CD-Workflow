93e05d2e0d009d97e80096341cc4903bfab4e5f3
Sayed-Ben-Slimene <sayed.benslimene@esprit.tn>
2024-08-01 13:49:11 +0100

    Build and publish

--- src/main/java/esprit/tn/revexamenpthologies/entities/Pathologie.java
+++ src/main/java/esprit/tn/revexamenpthologies/entities/Pathologie.java
@@ -25,7 +25,7 @@ public class Pathologie implements Serializable {
     private boolean archive;
     private boolean build;
 //test build
-//test buisld
+//test buisldhtf
 //test busdild
     @ManyToMany
     private Set<Acte> actes ;
