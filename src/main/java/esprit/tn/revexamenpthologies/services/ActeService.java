44d4c26d8fff99e4ae44ac406d62e571a0326715
Sayed-Ben-Slimene <sayed.benslimene@esprit.tn>
2024-08-02 03:06:46 +0100

    Update ActeService

--- src/main/java/esprit/tn/revexamenpthologies/services/ActeService.java
+++ src/main/java/esprit/tn/revexamenpthologies/services/ActeService.java
@@ -23,7 +23,7 @@ public class ActeService implements IActeService {
 
     @Autowired
     ActeRepository acteRepository;
-
+//trigger
     @Autowired
     PathologieRepository pathologieRepository;
     @Override
