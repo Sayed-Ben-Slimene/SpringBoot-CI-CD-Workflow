7d7723bf1c4e4bb0f1a1e6268de10857f99b4a11
Sayed-Ben-Slimene <sayed.benslimene@esprit.tn>
2024-08-02 08:30:15 +0100

    Building Test Update 2

--- src/main/java/esprit/tn/revexamenpthologies/services/ActeService.java
+++ src/main/java/esprit/tn/revexamenpthologies/services/ActeService.java
@@ -19,7 +19,7 @@ import java.util.Set;
 @NoArgsConstructor
 @Service
 @Slf4j
-public class ActeService implements IActeService {
+public class ActeService implements IActeSedrvice {
 
     @Autowired
     ActeRepository acteRepository;
