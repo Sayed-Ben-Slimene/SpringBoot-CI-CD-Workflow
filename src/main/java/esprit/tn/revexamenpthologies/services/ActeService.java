370f45f85519fc8b488ade88bf85f7d0b7385650
Sayed-Ben-Slimene <sayed.benslimene@esprit.tn>
2024-08-02 14:15:08 +0100

    Commit ActeModule

--- src/main/java/esprit/tn/revexamenpthologies/services/ActeService.java
+++ src/main/java/esprit/tn/revexamenpthologies/services/ActeService.java
@@ -23,7 +23,7 @@ public class ActeService implements IActeService {
 
     @Autowired
     ActeRepository acteRepository;
-//test
+//Merge test
     @Autowired
     PathologieRepository pathologieRepository;
     @Override
